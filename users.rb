30e3f6c64c0a58f0401b4edf367450310d9cd178
nikolaikotov <nikolaikotov4@gmail.com>
2020-10-13 17:08:20 +0300

    first commit

---
+++ users.rb
@@ -0,0 +1,29 @@
+class User
+  attr_accessor :name
+  attr_accessor :age
+  attr_accessor :email
+end
+
+list = Array.new
+
+user = User.new
+
+2.times do || 
+
+puts "Name:"
+user.name = gets.chomp.to_s
+
+puts "Age:"
+user.age = gets.chomp.to_s
+
+puts "Email:"
+user.email = gets.chomp.to_s
+
+puts "Name: #{user.name}, Age: #{user.age}, E-mail: #{user.email}"
+
+list << "#{user.name}, #{user.age}, #{user.email} \n"
+
+end
+
+
+puts list
