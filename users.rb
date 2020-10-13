2b6f14abf8e71f465d4393b1c3e2abda0425803b
nikolaikotov <nikolaikotov4@gmail.com>
2020-10-14 00:02:39 +0300

    final comit

--- users.rb
+++ users.rb
@@ -1,31 +1,31 @@
-class User
-  attr_accessor :name
-  attr_accessor :age
-  attr_accessor :email
-end
+	class User
+	  attr_accessor :name
+	  attr_accessor :age
+	  attr_accessor :email
+	end
 
-list = []
+	list = []
 
-user = User.new
+	user = User.new
 
-users_numbers = 10
+	users_numbers = 10
 
-while list.size < users_numbers do 
-	
-puts "Name:"
-user.name = gets.chomp.to_s
+	while list.size < users_numbers do 
+		
+		puts "Name:"
+		user.name = gets.chomp.to_s
 
-puts "Age:"
-user.age = gets.chomp.to_s
+		puts "Age:"
+		user.age = gets.chomp.to_s
 
-puts "Email:"
-user.email = gets.chomp.to_s
+		puts "Email:"
+		user.email = gets.chomp.to_s
 
-puts "Name: #{user.name}, Age: #{user.age}, E-mail: #{user.email}"
+		puts "Name: #{user.name}, Age: #{user.age}, E-mail: #{user.email}"
 
-list << "#{user.name}, #{user.age}, #{user.email} \n"
+		list << "#{user.name}, #{user.age}, #{user.email} \n"
 
-end
+	end
 
-puts list
+	puts list
 
